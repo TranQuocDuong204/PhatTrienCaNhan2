ae95026a62a0b80ffeb3382f2c2a04c456e82aaa
TranQuocDuong204 <tranquocduong2004vta@gmail.com>
2024-06-16 11:09:21 +0700

    Auto-generated commit

--- dist/nhom5/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-CQ5XCVG3.mjs";import"./chunk-IKJCG2K2.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
---
+++ nhom5/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-YKHXPC5D.mjs";import"./chunk-SFYUYNMJ.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
